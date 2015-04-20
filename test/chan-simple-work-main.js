b128ca31cdf0df80b9bd4971c8533f24e4a2ceca
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-04-21 07:31:46 +0900

    mocha test script - progress...

--- test/chan-simple-work-main.js
+++ test/chan-simple-work-main.js
@@ -1,34 +1,52 @@
 // test
 
-'use strict';
+describe('test chan simple', function () {
+  'use strict';
 
-var chan = require('../lib/chan-simple');
+  var chan = require('../lib/chan-simple');
+  var assert = require('assert');
 
-var ch = chan();
+  var ch = chan();
+  var act = [];
 
-function cb(err, val) {
-  if (err) {
-    console.log('recv: '+ err);
+  function cb(err, val) {
+    act.push([err, val]);
   }
-  else if (typeof val === 'object' && val !== null && ch.empty === val) {
-    console.log('recv: empty');
-  }
-  else {
-    console.log('recv: val = ' + val);
-  }
-} // cb
 
-ch(12);
-ch(34);
+  it('test', function () {
+    act = [];
 
-ch(cb);
-ch(cb);
-ch(cb);
+    ch(12);
+    assert.deepEqual(act, [], 'send 12');
+    ch(34);
+    assert.deepEqual(act, [], 'send 34');
 
-ch(56);
+    ch(cb);
+    assert.deepEqual(act.shift(), [null, 12], 'recv 12');
+    ch(cb);
+    assert.deepEqual(act.shift(), [null, 34], 'recv 34');
+    ch(cb);
+    assert.deepEqual(act, [], 'recv pending');
 
-ch.end();
-ch(cb);
-ch(cb);
+    ch(56);
+    assert.deepEqual(act.shift(), [null, 56], 'recv 56');
 
-// ch(78);
+    ch.end();
+    assert.deepEqual(act, [], 'recv pending');
+    ch(cb);
+    assert.deepEqual(act.shift(), [null, ch.empty], 'recv empty');
+    ch(cb);
+    assert.deepEqual(act.shift(), [null, ch.empty], 'recv empty');
+
+    var thru = false;
+    try {
+      ch(78);
+      thru = true;
+    } catch (err) {
+      assert.deepEqual(act, [], 'recv end');
+    }
+    if (thru) assert(false, 'send 78 fall thru');
+
+  }); // it
+
+}); // describe
--- test/chan-work-main.js
+++ test/chan-work-main.js
@@ -1,184 +1,321 @@
 // test
 
-'use strict';
+describe('test chan', function () {
+  'use strict';
 
-try {
-  var chan = require('co-chan');
-} catch (err) {
-  var chan = require('../lib/chan');
-}
+  var assert = require('assert');
 
-function cb(err, val) {
-  if (err) {
-    console.log('recv: '+ err);
+  try {
+    var chan = require('../lib/chan');
+  } catch (err) {
+    var chan = require('co-chan');
   }
-  else if (typeof val === 'object' && val !== null && ch.empty === val) {
-    console.log('recv: empty');
-  }
-  else {
-    console.log('recv: val = ' + val);
-  }
-} // cb
 
-function cb2(err, val) {
-  if (err) {
-    console.log('sent: '+ err);
-  }
-  else if (typeof val === 'object' && val !== null && ch.empty === val) {
-    console.log('sent: empty');
-  }
-  else {
-    console.log('sent: val = ' + val);
-  }
-} // cb2
+  var act1 = [];
+  var act2 = [];
 
-var ch = chan();
-console.log('ch 1');
-ch(1); // send
-console.log('ch 2');
-ch(2); // send
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch 3');
-ch(3); // send
-console.log('ch.end');
-ch.end(); // end
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-try {
-  console.log('ch 4');
-  ch(4); // send
-} catch(err) {
-  console.log(err + ': 4 cant send')
-}
-console.log();
+  function cb(err, val) {
+    act1.push([err, val]);
+    /*
+    if (err)
+      console.log('recv: '+ err);
+    else if (val && typeof val === 'object' && ch.empty === val)
+      console.log('recv: empty');
+    else
+      console.log('recv: val = ' + val);
+    */
+  } // cb
 
-var ch = chan();
-console.log('ch 1');
-ch(1)(cb2); // send
-console.log('ch 2');
-ch(2)(cb2); // send
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch 3');
-ch(3)(cb2); // send
-console.log('ch.end');
-ch.end(); // end
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-try {
-  console.log('ch 4');
-  ch(4)(cb2); // send
-} catch(err) {
-  console.log(err + ': 4 cant send')
-}
-console.log();
+  function cb2(err, val) {
+    act2.push([err, val]);
+    /*
+    if (err)
+      console.log('sent: '+ err);
+    else if (val && typeof val === 'object' && ch.empty === val)
+      console.log('sent: empty');
+    else
+      console.log('sent: val = ' + val);
+    */
+  } // cb2
 
-var ch = chan(null, 1);
-console.log('ch 1');
-ch(1)(cb2); // send
-console.log('ch 2');
-ch(2)(cb2); // send
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch 3');
-ch(3)(cb2); // send
-console.log('ch.end');
-ch.end(); // end
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log();
+  it('test 1 channel send async', function () {
 
-var ch = chan(null, 2);
-console.log('ch 1');
-ch(1)(cb2); // send
-console.log('ch 2');
-ch(2)(cb2); // send
-console.log('ch 3');
-ch(3)(cb2); // send
-console.log('ch 4');
-ch(4)(cb2); // send
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch 5');
-ch(5)(cb2); // send
-console.log('ch 6');
-ch(6)(cb2); // send
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch.end');
-ch.end(); // end
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log();
+    act1 = [];
 
+    var ch = chan();
+    // console.log('ch 1');
+    ch(1); // send
+    assert.deepEqual(act1, [], 'send 1');
 
-var ch = chan(null, Infinity);
-console.log('ch 1');
-ch(1)(cb2); // send
-console.log('ch 2');
-ch(2)(cb2); // send
-console.log('ch 3');
-ch(3)(cb2); // send
-console.log('ch 4');
-ch(4)(cb2); // send
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch 5');
-ch(5)(cb2); // send
-console.log('ch 6');
-ch(6)(cb2); // send
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log('ch.end');
-ch.end(); // end
-console.log('ch');
-ch(cb); // recv
-console.log('ch');
-ch(cb); // recv
-console.log();
+    // console.log('ch 2');
+    ch(2); // send
+    assert.deepEqual(act1, [], 'send 2');
 
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, 1], 'recv 1');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, 2], 'recv 2');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1, [], 'recv 3 pending');
+
+    // console.log('ch 3');
+    ch(3); // send
+    assert.deepEqual(act1.shift(), [null, 3], 'recv 3');
+
+    // console.log('ch.end');
+    ch.end(); // end
+    assert.deepEqual(act1, [], 'send end');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, ch.empty], 'recv empty 1');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, ch.empty], 'recv empty 2');
+
+    var thru = false;
+    try {
+      // console.log('ch 4');
+      ch(4); // send
+      thru = true;
+    } catch(err) {
+      // console.log(err + ': 4 cant send');
+    }
+    if (thru) assert(false, 'send 4 fall thru');
+    // console.log();
+
+  }); // it
+
+  it('test 1 channel send sync size 0', function () {
+
+    act1 = [];
+    act2 = [];
+
+    var ch = chan();
+
+    // console.log('ch 1');
+    ch(1)(cb2); // send
+    assert.deepEqual(act2, [], 'send 1 pending');
+
+    // console.log('ch 2');
+    ch(2)(cb2); // send
+    assert.deepEqual(act2, [], 'send 2 pending');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, 1], 'recv 1');
+    assert.deepEqual(act2.shift(), [null, 1], 'send 1');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, 2], 'recv 2');
+    assert.deepEqual(act2.shift(), [null, 2], 'send 2');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1, [], 'recv 3 pending');
+
+    // console.log('ch 3');
+    ch(3)(cb2); // send
+    assert.deepEqual(act2.shift(), [null, 3], 'send 3');
+    assert.deepEqual(act1.shift(), [null, 3], 'recv 3');
+
+    // console.log('ch.end');
+    ch.end(); // end
+    assert.deepEqual(act1, [], 'recv end pending');
+    assert.deepEqual(act2, [], 'send end pending');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, null], 'recv end');
+
+    // console.log('ch');
+    ch(cb); // recv
+    assert.deepEqual(act1.shift(), [null, null], 'recv end');
+
+    var thru = false;
+    try {
+      // console.log('ch 4');
+      ch(4)(cb2); // send
+      thru = true;
+    } catch(err) {
+      // console.log(err + ': 4 cant send')
+    }
+    if (thru) assert(false, 'send 4 fall thru');
+    // console.log();
+    assert.deepEqual(act1, [], 'recv end pending');
+    assert.deepEqual(act2, [], 'send end pending');
+
+  }); // it
+
+  it('test 1 channel send sync size 1', function () {
+
+    act1 = [];
+    act2 = [];
+
+    console.log('1#%j 2#%j', act1, act2);
+
+    var ch = chan(null, 1);
+
+    // console.log('ch 1');
+    ch(1)(cb2); // send
+
+    // console.log('ch 2');
+    ch(2)(cb2); // send
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch 3');
+    ch(3)(cb2); // send
+
+    // console.log('ch.end');
+    ch.end(); // end
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log();
+
+  }); // it
+
+  it('test 1 channel send sync size 2', function () {
+
+    act1 = [];
+    act2 = [];
+
+    var ch = chan(null, 2);
+
+    // console.log('ch 1');
+    ch(1)(cb2); // send
+
+    // console.log('ch 2');
+    ch(2)(cb2); // send
+
+    // console.log('ch 3');
+    ch(3)(cb2); // send
+
+    // console.log('ch 4');
+    ch(4)(cb2); // send
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch 5');
+    ch(5)(cb2); // send
+
+    // console.log('ch 6');
+    ch(6)(cb2); // send
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch.end');
+    ch.end(); // end
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log();
+
+  }); // it
+
+  it('test 1 channel send sync size Infinity', function () {
+
+    act1 = [];
+    act2 = [];
+
+    var ch = chan(null, Infinity);
+
+    // console.log('ch 1');
+    ch(1)(cb2); // send
+
+    // console.log('ch 2');
+    ch(2)(cb2); // send
+
+    // console.log('ch 3');
+    ch(3)(cb2); // send
+
+    // console.log('ch 4');
+    ch(4)(cb2); // send
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch 5');
+    ch(5)(cb2); // send
+
+    // console.log('ch 6');
+    ch(6)(cb2); // send
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch.end');
+    ch.end(); // end
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log('ch');
+    ch(cb); // recv
+
+    // console.log();
+
+  }); // it
+
+}); // describe
